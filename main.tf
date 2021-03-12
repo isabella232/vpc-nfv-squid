de50fcc794da46d8037f59adf9db92375d5337a8
Powell Quiring <powellquiring@gmail.com>
2021-03-12 10:49:32 -0800

    terraform fmt

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "ibm_resource_group" "group" {
 
 locals {
   # myip = data.external.ifconfig_me.result.ip # ratched down to just your workstation if needed
-  myip    = "0.0.0.0/0"
+  myip = "0.0.0.0/0"
 
   vpc_cidr = "10.0.0.0/14"
 
@@ -30,18 +30,18 @@ locals {
   }
 
   proxy_zone_index = 0 # zone for proxy
-  proxy_zone = ibm_is_vpc_address_prefix.prefixes[local.proxy_zone_index].zone
-  proxy_cidr = cidrsubnet(ibm_is_vpc_address_prefix.prefixes[local.proxy_zone_index].cidr, 8, 1)
+  proxy_zone       = ibm_is_vpc_address_prefix.prefixes[local.proxy_zone_index].zone
+  proxy_cidr       = cidrsubnet(ibm_is_vpc_address_prefix.prefixes[local.proxy_zone_index].cidr, 8, 1)
 
-  jump_zone_index  = 0 # zone for jump
-  jump_zone = ibm_is_vpc_address_prefix.prefixes[local.jump_zone_index].zone
-  jump_cidr = cidrsubnet(ibm_is_vpc_address_prefix.prefixes[local.jump_zone_index].cidr, 8, 2)
+  jump_zone_index = 0 # zone for jump
+  jump_zone       = ibm_is_vpc_address_prefix.prefixes[local.jump_zone_index].zone
+  jump_cidr       = cidrsubnet(ibm_is_vpc_address_prefix.prefixes[local.jump_zone_index].cidr, 8, 2)
 }
 
 #----- vpc, address prefix 
 resource "ibm_is_vpc" "vpc" {
-  name           = var.basename
-  resource_group = data.ibm_resource_group.group.id
+  name                      = var.basename
+  resource_group            = data.ibm_resource_group.group.id
   address_prefix_management = "manual"
 }
 
@@ -77,7 +77,7 @@ resource "ibm_is_security_group_rule" "outbound_all" {
 resource "ibm_is_security_group_rule" "inbound_all" {
   group     = ibm_is_security_group.sg1.id
   direction = "inbound"
-  remote     = ibm_is_security_group.sg1.id
+  remote    = ibm_is_security_group.sg1.id
 }
 
 # allow ssh access to the jump server from your desktop
@@ -98,18 +98,18 @@ resource "ibm_is_security_group_rule" "ingress_ssh_all" {
 
 #----- squid proxy
 resource "ibm_is_public_gateway" "proxy" {
-  name           = "${var.basename}-proxy"
-  vpc            = ibm_is_vpc.vpc.id
-  zone           = local.proxy_zone
+  name = "${var.basename}-proxy"
+  vpc  = ibm_is_vpc.vpc.id
+  zone = local.proxy_zone
 }
 
 resource "ibm_is_subnet" "proxy" {
   name            = "${var.basename}-proxy"
   vpc             = ibm_is_vpc.vpc.id
-  zone           = local.proxy_zone
+  zone            = local.proxy_zone
   ipv4_cidr_block = local.proxy_cidr
   resource_group  = data.ibm_resource_group.group.id
-  public_gateway = ibm_is_public_gateway.proxy.id
+  public_gateway  = ibm_is_public_gateway.proxy.id
 }
 
 resource "ibm_is_instance" "proxy" {
@@ -137,19 +137,11 @@ output "sshproxy" {
   value = "ssh -J root@${ibm_is_floating_ip.jump.address} root@${ibm_is_instance.proxy.primary_network_interface[0].primary_ipv4_address}"
 }
 
-/*-----
-resource "ibm_is_floating_ip" "proxy" {
-  name           = "${var.basename}-proxy"
-  target         = ibm_is_instance.proxy.primary_network_interface[0].id
-  resource_group = data.ibm_resource_group.group.id
-}
------*/
-
 #----
 resource "ibm_is_subnet" "jump" {
-  name           = "${var.basename}-jump"
+  name            = "${var.basename}-jump"
   vpc             = ibm_is_vpc.vpc.id
-  zone           = local.jump_zone
+  zone            = local.jump_zone
   ipv4_cidr_block = local.jump_cidr
   resource_group  = data.ibm_resource_group.group.id
 }
@@ -204,7 +196,7 @@ resource "ibm_is_subnet" "hosts" {
 # address for cloud service endpoints for dns server, timeserver, resources, ... defaults are good
 # https://cloud.ibm.com/docs/vpc-on-classic?topic=vpc-on-classic-service-endpoints-available-for-ibm-cloud-vpc
 resource "ibm_is_vpc_routing_table_route" "ibm_cloud_service_endpoints_161" {
-  for_each = ibm_is_subnet.hosts
+  for_each      = ibm_is_subnet.hosts
   vpc           = ibm_is_vpc.vpc.id
   routing_table = ibm_is_vpc_routing_table.vpc.routing_table
   zone          = each.value.zone
@@ -214,7 +206,7 @@ resource "ibm_is_vpc_routing_table_route" "ibm_cloud_service_endpoints_161" {
 }
 
 resource "ibm_is_vpc_routing_table_route" "ibm_cloud_service_endpoints_166" {
-  for_each = ibm_is_subnet.hosts
+  for_each      = ibm_is_subnet.hosts
   vpc           = ibm_is_vpc.vpc.id
   routing_table = ibm_is_vpc_routing_table.vpc.routing_table
   zone          = each.value.zone
@@ -225,7 +217,7 @@ resource "ibm_is_vpc_routing_table_route" "ibm_cloud_service_endpoints_166" {
 
 # address explicitly for the proxy - send to proxy
 resource "ibm_is_vpc_routing_table_route" "host_within_vpc" {
-  for_each = ibm_is_subnet.hosts
+  for_each      = ibm_is_subnet.hosts
   vpc           = ibm_is_vpc.vpc.id
   routing_table = ibm_is_vpc_routing_table.vpc.routing_table
   zone          = each.value.zone
@@ -236,7 +228,7 @@ resource "ibm_is_vpc_routing_table_route" "host_within_vpc" {
 
 # all other addresses, go to the proxy
 resource "ibm_is_vpc_routing_table_route" "host_to_proxy_all" {
-  for_each = ibm_is_subnet.hosts
+  for_each      = ibm_is_subnet.hosts
   vpc           = ibm_is_vpc.vpc.id
   routing_table = ibm_is_vpc_routing_table.vpc.routing_table
   zone          = each.value.zone
@@ -246,31 +238,30 @@ resource "ibm_is_vpc_routing_table_route" "host_to_proxy_all" {
 }
 
 resource "ibm_is_instance" "host" {
-  for_each = ibm_is_subnet.hosts
+  for_each       = ibm_is_subnet.hosts
   name           = each.value.name
   vpc            = ibm_is_vpc.vpc.id
-  zone          = each.value.zone
+  zone           = each.value.zone
   keys           = [data.ibm_is_ssh_key.ssh_key.id]
   image          = data.ibm_is_image.ubuntu.id
   profile        = var.profile
   resource_group = data.ibm_resource_group.group.id
   primary_network_interface {
     subnet          = each.value.id
-    security_groups   = [ibm_is_security_group.sg1.id]
+    security_groups = [ibm_is_security_group.sg1.id]
   }
 }
-
 
 output "host" {
   value = [
     #for index, instance in ibm_is_instance.host: index => {
-    for index, instance in ibm_is_instance.host: {
+    for index, instance in ibm_is_instance.host : {
       ip_host = instance.primary_network_interface[0].primary_ipv4_address,
       sshhost = "ssh -J root@${ibm_is_floating_ip.jump.address} root@${instance.primary_network_interface[0].primary_ipv4_address}",
     }
   ]
 }
 
-output basename {
+output "basename" {
   value = var.basename
 }
