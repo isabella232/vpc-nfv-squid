ffbab21953e1342d365d006f43f14b5725a93ac5
Powell Quiring <powellquiring@gmail.com>
2021-03-15 16:25:24 -0700

    broke out existing terraform into version.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_version = ">= 0.14"
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-      version = ">= 1.20"
-    }
-  }
-}
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
---
+++ version.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "> 1.0"
+    }
+  }
+}
+
